79bff88c834a3ec9a9443b8cefbf8cb5b10bcd02
Jonas Diete <jonas.diete@gmail.com>
2023-04-18 10:44:59 +0100

    all board size tests pass

--- test_board.py
+++ test_board.py
@@ -1,5 +1,13 @@
 from board import Board
 
+def test_board_creates_size_two():
+    board = Board(2)
+    assert board.rows == [["O", "O"], ["O", "O"]]
+
 def test_board_creates_size_four():
     board = Board(4)
     assert board.rows == [["O", "O", "O", "O"], ["O", "O", "O", "O"], ["O", "O", "O", "O"], ["O", "O", "O", "O"]]
+
+def test_board_creates_size_seven():
+    board = Board(7)
+    assert board.rows == [["O", "O", "O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O", "O", "O"], ["O", "O", "O", "O", "O", "O", "O"]]
